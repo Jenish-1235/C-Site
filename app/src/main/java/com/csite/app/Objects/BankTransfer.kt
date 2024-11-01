e24e7a267ac7954aeceeefab50a20be897e26253
Jenish Togadiya <jenishtogadiya549@gmail.com>
2024-11-02 02:35:06 +0530

    feature - save new bank transfers to firebase database.

---
+++ app/src/main/java/com/csite/app/Objects/BankTransfer.kt
@@ -0,0 +1,21 @@
+package com.csite.app.Objects
+
+class BankTransfer {
+    var date: String = ""
+    var partyName: String = ""
+    var amount: String = ""
+    var accountNumber: String = "" ;
+    var head: String = "";
+    var doneBy: String = "";
+    var site: String = "";
+    constructor(date: String, partyName: String, accountNumber: String, amount: String, head: String, doneBy: String, site: String) {
+        this.date = date;
+        this.partyName = partyName;
+        this.amount = amount;
+        this.accountNumber = accountNumber;
+        this.head = head;
+        this.doneBy = doneBy;
+        this.site = site;
+    }
+
+}
