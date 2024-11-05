0a2a2a6701273d8dae4c52a48f0b866720114017
Jenish Togadiya <jenishtogadiya549@gmail.com>
2024-11-06 04:20:07 +0530

    feature-adding-of-workers-works-good

--- app/src/main/java/com/csite/app/FirebaseOperations/FirebaseOperationsForProjectInternalAttendance.kt
+++ app/src/main/java/com/csite/app/FirebaseOperations/FirebaseOperationsForProjectInternalAttendance.kt
@@ -1,10 +1,17 @@
 package com.csite.app.FirebaseOperations
 
+import android.util.Log
+import android.widget.Toast
+import androidx.core.content.ContentProviderCompat.requireContext
 import com.csite.app.Objects.ProjectWorker
+import com.google.firebase.database.ChildEventListener
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.DatabaseReference.CompletionListener
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.database.values
 
 class FirebaseOperationsForProjectInternalAttendance {
     object FirebaseOperationsForProjectInternalAttendance{}
@@ -15,6 +22,7 @@ class FirebaseOperationsForProjectInternalAttendance {
         projectReference.child(projectId).child("ProjectWorkers").setValue(workersList)
     }
 
+    // 1.1 Fetch workerlist from firebase
     fun fetchProjectWorkers(projectId:String, callback: OnProjectWorkersFetched) {
         val workersList = ArrayList<ProjectWorker>()
         projectReference.child(projectId).child("ProjectWorkers").addValueEventListener(object : ValueEventListener{
@@ -34,9 +42,82 @@ class FirebaseOperationsForProjectInternalAttendance {
 
         })
     }
-
     interface OnProjectWorkersFetched {
         fun onProjectWorkersFetched(workersList: ArrayList<ProjectWorker>)
     }
+    // 3. Check if workers exist in project.
+    fun checkWorkerExist(projectId: String, callback:(Boolean) -> Unit) {
+        projectReference.child(projectId).child("ProjectWorkers").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val workersExist = snapshot.exists()
+                callback(workersExist)
+            }
 
+            override fun onCancelled(error: DatabaseError) {
+                callback(false)
+            }
+        })
+    }
+    // 4. Check if attendance exists
+    fun checkAttendanceExists(projectId: String, callback: (Boolean) -> Unit) {
+        projectReference.child(projectId).child("ProjectAttendance").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val attendanceExists = snapshot.exists()
+                callback(attendanceExists)
+            }
+            override fun onCancelled(error: DatabaseError) {
+                callback(false)
+            }
+        })
+    }
+
+    fun saveAttendance(projectId: String, date: String, attendance: HashMap<String, ProjectWorker>) {
+        val hashMapWithDate = HashMap<String, HashMap<String, ProjectWorker>>()
+        hashMapWithDate[date] = attendance
+        val finalHashMap = HashMap<String, Any>()
+        finalHashMap["ProjectAttendance"] = hashMapWithDate
+        projectReference.child(projectId).updateChildren(finalHashMap as Map<String, Any>)
+    }
+
+    fun updateAttendance(projectId: String, date: String, attendance: HashMap<String, ProjectWorker>) {
+        val hashMapWithDate = HashMap<String, HashMap<String, ProjectWorker>>()
+        hashMapWithDate[date] = attendance
+        projectReference.child(projectId).child("ProjectAttendance").updateChildren(hashMapWithDate as Map<String, Any>)
+    }
+
+    fun checkDateExists(projectId: String, date: String, callback: (Boolean) -> Unit) {
+        projectReference.child(projectId).child("ProjectAttendance").addListenerForSingleValueEvent(object : ValueEventListener {
+            override fun onDataChange(snapshot: DataSnapshot) {
+                val dateExists = snapshot.hasChild(date)
+                callback(dateExists)
+                }
+            override fun onCancelled(error: DatabaseError) {
+                callback(false)
+            }
+        })
+    }
+
+    fun getWorkersForExistingAttendance(projectId: String, date: String,callback: OnAttendanceWorkerFetched){
+        val workersList = ArrayList<ProjectWorker>()
+        projectReference.child(projectId).child("ProjectAttendance").child(date).addValueEventListener(object : ValueEventListener{
+            override fun onDataChange(snapshot: DataSnapshot) {
+                workersList.clear()
+                for (workerSnapshot in snapshot.children) {
+                    val worker = workerSnapshot.getValue(ProjectWorker::class.java)
+                    if (worker != null) {
+                        workersList.add(worker)
+                    }
+                }
+                callback.onAttendanceWorkerFetched(workersList)
+            }
+
+            override fun onCancelled(error: DatabaseError) {
+
+            }
+        })
+    }
+
+    interface OnAttendanceWorkerFetched{
+        fun onAttendanceWorkerFetched(workersList: ArrayList<ProjectWorker>)
+    }
 }
--- app/src/main/java/com/csite/app/Fragments/ProjectInternal/ProjectInternalAttendanceFragment.kt
+++ app/src/main/java/com/csite/app/Fragments/ProjectInternal/ProjectInternalAttendanceFragment.kt
@@ -1,7 +1,7 @@
 package com.csite.app.Fragments.ProjectInternal
 
 import android.content.Intent
-import android.media.Image
+import android.content.res.ColorStateList
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,54 +10,49 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.csite.app.Activites.ProjectFeatures.AttendanceTab.AddWorkersToProjectActivity
 import com.csite.app.FirebaseOperations.FirebaseOperationsForProjectInternalAttendance
 import com.csite.app.Objects.ProjectWorker
 import com.csite.app.R
 import com.csite.app.RecyclerViewListAdapters.AttendanceListAdapter
 import com.google.android.material.floatingactionbutton.FloatingActionButton
-import java.text.DateFormat
-import java.text.SimpleDateFormat
+import com.google.firebase.database.FirebaseDatabase
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import java.util.Calendar
-import java.util.Date
+import kotlin.collections.HashMap
 
 
 class ProjectInternalAttendanceFragment : Fragment() {
-
+    val pref = FirebaseDatabase.getInstance().getReference("Projects")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+
         val view =  inflater.inflate(R.layout.fragment_project_internal_attendance, container, false)
 
         val bundle = getArguments()
-        var projectId = bundle?.getString("projectId")
+        val projectId = bundle?.getString("projectId")
         val memberAccess = bundle?.getString("memberAccess")
+
+        val attendanceRecyclerView = view.findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.projectWorkersRecyclerView)
+        val emptyWorkerList = ArrayList<ProjectWorker>()
+        val adapter = AttendanceListAdapter(emptyWorkerList)
+        attendanceRecyclerView.adapter = adapter
+        attendanceRecyclerView.layoutManager = LinearLayoutManager(requireActivity())
+
+
+        val addDayButton = view.findViewById<Button>(R.id.addDayButton)
+        addDayButton.text = "Add Day"
+        addDayButton.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.yellow))
 
         val currentDate = getCurrentDate()
         val selectedDateTextView = view.findViewById<TextView>(R.id.selectedDateView)
         selectedDateTextView.text = currentDate
-
-        val yesterdayDateButton:ImageView = view.findViewById(R.id.yesterdayDateButton)
-        yesterdayDateButton.setOnClickListener{
-            var selectedDate = selectedDateTextView.text.toString()
-            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-            val date = LocalDate.parse(selectedDate, formatter)
-            val yesterday = date.minusDays(1)
-            selectedDateTextView.text = yesterday.format(formatter)
-        }
-
-        val tomorrowDateButton:ImageView = view.findViewById(R.id.tomorrowDateButton)
-        tomorrowDateButton.setOnClickListener {
-            var selectedDate = selectedDateTextView.text.toString()
-            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
-            val date = LocalDate.parse(selectedDate, formatter)
-            val tomorrow = date.plusDays(1)
-            selectedDateTextView.text = tomorrow.format(formatter)
-        }
 
 
         val addWorkerButton: FloatingActionButton = view.findViewById(R.id.addWorkersButton)
@@ -68,23 +63,89 @@ class ProjectInternalAttendanceFragment : Fragment() {
             startActivity(addWorkerToProjectIntent)
         }
 
-        val attendanceRecyclerView = view.findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.projectWorkersRecyclerView)
-        attendanceRecyclerView.layoutManager = androidx.recyclerview.widget.LinearLayoutManager(requireActivity())
-
         val firebaseOperationsForProjectInternalAttendance = FirebaseOperationsForProjectInternalAttendance()
-        if (projectId != null) {
-            firebaseOperationsForProjectInternalAttendance.fetchProjectWorkers(projectId, object : FirebaseOperationsForProjectInternalAttendance.OnProjectWorkersFetched {
-                override fun onProjectWorkersFetched(workersList: ArrayList<ProjectWorker>) {
-                    val adapter = AttendanceListAdapter(workersList)
-                    attendanceRecyclerView.adapter = adapter
-                    adapter.notifyDataSetChanged()
-                }
 
-            })
+        val yesterdayDateButton:ImageView = view.findViewById(R.id.yesterdayDateButton)
+        yesterdayDateButton.setOnClickListener{
+            var selectedDate = selectedDateTextView.text.toString()
+            val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+            val date = LocalDate.parse(selectedDate, formatter)
+            val yesterday = date.minusDays(1)
+            selectedDateTextView.text = yesterday.format(formatter)
+            if (projectId != null) {
+                updateUI(view,projectId)
+            }
+
+        }
+
+        val tomorrowDateButton:ImageView = view.findViewById(R.id.tomorrowDateButton)
+        tomorrowDateButton.setOnClickListener {
+            var selectedDate = selectedDateTextView.text.toString()
+            if (selectedDate != getCurrentDate()){
+                val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
+                val date = LocalDate.parse(selectedDate, formatter)
+                val tomorrow = date.plusDays(1)
+                selectedDateTextView.text = tomorrow.format(formatter)
+                if (projectId != null) {
+                    updateUI(view,projectId)
+                }
+            }else{
+                if (projectId != null) {
+                    updateUI(view, projectId)
+                }
+            }
+        }
+
+        if (projectId != null) {
+            updateUI(view, projectId)
+        }
+
+        addDayButton.setOnClickListener{
+            if (projectId != null) {
+                firebaseOperationsForProjectInternalAttendance.checkWorkerExist(projectId){exist->
+                    if (exist){
+                        addWorkerButton.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.green))
+                        var projectWorkerList = ArrayList<ProjectWorker>()
+                        firebaseOperationsForProjectInternalAttendance.fetchProjectWorkers(projectId,
+                            object : FirebaseOperationsForProjectInternalAttendance.OnProjectWorkersFetched {
+                                override fun onProjectWorkersFetched(workersList: ArrayList<ProjectWorker>) {
+                                    projectWorkerList = workersList
+                                    firebaseOperationsForProjectInternalAttendance.checkAttendanceExists(projectId){attendanceExist ->
+                                        if (attendanceExist){
+                                            val attendanceHashMap = HashMap<String, ProjectWorker>()
+                                            for (worker in projectWorkerList){
+                                                attendanceHashMap.put(worker.wId,worker)
+                                            }
+                                            firebaseOperationsForProjectInternalAttendance.updateAttendance(projectId,selectedDateTextView.text.toString(),attendanceHashMap)
+                                            updateUI(view,projectId)
+                                        }else{
+                                            val attendanceHashMap = HashMap<String, ProjectWorker>()
+                                            for (worker in projectWorkerList){
+                                                attendanceHashMap.put(worker.wId,worker)
+                                            }
+                                            firebaseOperationsForProjectInternalAttendance.saveAttendance(projectId,selectedDateTextView.text.toString(),attendanceHashMap)
+                                        }
+                                    }
+                                }
+                            })
+                    }else{
+                        Toast.makeText(requireActivity(), "No Workers Found", Toast.LENGTH_SHORT).show()
+                        val addWorkerToProjectIntent = Intent(activity, AddWorkersToProjectActivity::class.java)
+                        addWorkerToProjectIntent.putExtra("projectId",projectId)
+                        addWorkerToProjectIntent.putExtra("memberAccess",memberAccess)
+                        startActivity(addWorkerToProjectIntent)
+                    }
+                }
+            }
         }
 
 
+        val saveAttendanceButton:Button = view.findViewById(R.id.saveAttendanceButton)
+        saveAttendanceButton.setOnClickListener{
+            if (projectId != null) {
 
+            }
+        }
         return view
     }
 
@@ -93,6 +154,61 @@ class ProjectInternalAttendanceFragment : Fragment() {
         val today = LocalDate.now()
         val formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy")
         return today.format(formatter)
+    }
+
+    fun updateUI(view: View,projectId:String){
+        val firebaseOperationsForProjectInternalAttendance = FirebaseOperationsForProjectInternalAttendance()
+        val addWorkerButton: FloatingActionButton = view.findViewById(R.id.addWorkersButton)
+        val addDayButton = view.findViewById<Button>(R.id.addDayButton)
+        val selectedDateTextView = view.findViewById<TextView>(R.id.selectedDateView)
+        val attendanceRecyclerView = view.findViewById<androidx.recyclerview.widget.RecyclerView>(R.id.projectWorkersRecyclerView)
+        attendanceRecyclerView.layoutManager = LinearLayoutManager(requireActivity())
+        val emptyWorkerList = ArrayList<ProjectWorker>()
+        val adapter = AttendanceListAdapter(emptyWorkerList)
+        attendanceRecyclerView.adapter = adapter
+
+        if (projectId != null) {
+            firebaseOperationsForProjectInternalAttendance.checkWorkerExist(projectId){workerExist ->
+                if (workerExist){
+
+                    addWorkerButton.setBackgroundColor(getResources().getColor(R.color.green))
+                    addWorkerButton.setImageDrawable(getResources().getDrawable(R.drawable.icon_edit))
+
+                    firebaseOperationsForProjectInternalAttendance.checkAttendanceExists(projectId){attendanceExist ->
+                        if (attendanceExist){
+                            firebaseOperationsForProjectInternalAttendance.checkDateExists(projectId,selectedDateTextView.text.toString()){dateExist ->
+                                if (dateExist){
+                                    addDayButton.text = "Update Workers"
+                                    firebaseOperationsForProjectInternalAttendance.getWorkersForExistingAttendance(projectId, selectedDateTextView.text.toString(), object : FirebaseOperationsForProjectInternalAttendance.OnAttendanceWorkerFetched {
+                                        override fun onAttendanceWorkerFetched(workersList: ArrayList<ProjectWorker>) {
+                                            val adapter = AttendanceListAdapter(workersList)
+                                            attendanceRecyclerView.adapter = adapter
+                                            adapter.notifyDataSetChanged()
+                                        }
+                                    })
+                                }else{
+                                    addDayButton.text = "Add Day"
+                                    addDayButton.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.yellow))
+                                }
+                            }
+                        }else{
+                            addDayButton.text = "Add Day"
+                            addDayButton.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.yellow))
+                        }
+                    }
+
+                }else{
+
+                    Toast.makeText(requireActivity(), "No Workers Found", Toast.LENGTH_SHORT).show()
+                    addDayButton.text = "Add Day"
+
+                    addDayButton.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.yellow))
+                    addWorkerButton.backgroundTintList = ColorStateList.valueOf(resources.getColor(R.color.yellow))
+                    addWorkerButton.setImageDrawable(getResources().getDrawable(R.drawable.party_icon_black))
+                }
+            }
+        }
+
     }
 
 
