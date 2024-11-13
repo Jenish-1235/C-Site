545d985b326bd9341eabd8a2dd2cc182a70e41c9
Jenish Togadiya <jenishtogadiya549@gmail.com>
2024-11-13 10:23:55 +0530

    fix-party-external-fragment

--- app/src/main/java/com/csite/app/Fragments/MainScreen/PartyFragment.kt
+++ app/src/main/java/com/csite/app/Fragments/MainScreen/PartyFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.csite.app.Activites.Library.AddNewPartyActivity
@@ -72,7 +73,12 @@ class PartyFragment : Fragment() {
                                                         }
                                                     }
                                                 }
-                                                paymentsHashMap[party.partyName] = amount
+                                                try{
+
+                                                    paymentsHashMap[party.partyName] = paymentsHashMap[party.partyName]!!.toDouble() + amount
+                                                }catch (e: Exception){
+                                                    paymentsHashMap[party.partyName] = amount
+                                                }
                                                 var adapter = PaymentsListAdapter(paymentsHashMap)
                                                 externalPartiesTabRecyclerView.adapter = adapter
                                                 externalPartiesTabRecyclerView.setHasFixedSize(true)
