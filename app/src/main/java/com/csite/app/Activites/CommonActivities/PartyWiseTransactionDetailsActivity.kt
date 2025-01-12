109cab5509398c2c0a1a373a552cab2ec063f8b6
Jenish Togadiya <jenishtogadiya549@gmail.com>
2025-01-12 18:55:09 +0530

    fix-coupling-between-code-to-send-partyid

---
+++ app/src/main/java/com/csite/app/Activites/CommonActivities/PartyWiseTransactionDetailsActivity.kt
@@ -0,0 +1,20 @@
+package com.csite.app.Activites.CommonActivities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.csite.app.R
+
+class PartyWiseTransactionDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_party_wise_transaction_details)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
